222f045758fa0342ad0b98197285456d62bf1864
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-10-16 17:53:05 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int res[14] = {0,2,7,5,30,169,441,1872,7632,1740,93313,459901,1358657,2504881};
     int x;
-    while(cin >> x){
+    while(cin >> x,x!=0){
         cout << res[x-1] << endl;
     }
 }
