aec09b06227db03f26291be236c6d1ee9bc54672
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-10-16 17:51:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-
-
 int main(){
-
-
-
-
-
-
-
-
-
+    int res[14] = {0,2,7,5,30,169,441,1872,7632,1740,93313,459901,1358657,2504881};
+    int x;
+    while(cin >> x){
+        cout << res[x-1] << endl;
+    }
 }
